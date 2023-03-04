9b4a1996bd45ad96e603f7de03500ff24a7251a6
moh <moh94syed@gmail.com>
2023-03-04 12:17:26 +0200

    a

--- lib/views/nawawi/nawawi_view.dart
+++ lib/views/nawawi/nawawi_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quran/tools/constants.dart';
+import 'package:quran/tools/push.dart';
+import 'package:quran/views/nawawi/nawawi_model.dart';
 import 'package:quran/views/nawawi/nawawi_provider.dart';
 import 'package:quran/widgets/app_bar_title.dart';
+import 'package:quran/widgets/btn.dart';
 import 'package:quran/widgets/custom_card.dart';
 import 'package:quran/widgets/custom_loading.dart';
 import 'package:quran/widgets/icon_leading.dart';
@@ -44,8 +47,10 @@ class NawawisView extends StatelessWidget {
                       fontSize: 18.0,
                     ),
                     trailing: IconButton(
-                      onPressed: () {},
                       icon: const Icon(Icons.arrow_forward_ios),
+                      onPressed: () {
+                        Push.to(context, DescriptionWidget(nawawi: nawawy));
+                      },
                     ),
                   ),
                 ),
@@ -53,6 +58,55 @@ class NawawisView extends StatelessWidget {
             },
           );
         },
+      ),
+    );
+  }
+}
+
+class DescriptionWidget extends StatelessWidget {
+  const DescriptionWidget({Key? key, required this.nawawi}) : super(key: key);
+  final NawawiModel nawawi;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: [
+          CustomCard(child: Txt(nawawi.hadith)),
+          Btn(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                // isScrollControlled: true,
+                builder: (context) {
+                  return Scrollbar(
+                    child: ListView(
+                      padding: const EdgeInsets.all(28.0),
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        Txt(
+                          nawawi.description,
+                          isUseFontSizePrefs: true,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            margin: const EdgeInsets.all(20.0),
+            child: const Txt(
+              'شرح الحديث',
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
